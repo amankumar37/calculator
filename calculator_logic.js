1ba2bfe8f0d50f0c4bdd2bcde49589ba6e0f7945
amankumar37 <115777960+amankumar37@users.noreply.github.com>
2023-01-14 09:35:46 +0000

    JS file uploaded

---
+++ calculator_logic.js
@@ -0,0 +1,34 @@
+var display= document.getElementById('display');
+var buttons=document.getElementsByClassName('buttons');
+var operator=null;
+var operand1=0;
+var operand2=null; 
+display.innerText="";
+
+
+for(var i=0;i<buttons.length;i++){
+      let text=buttons[i].innerText;
+      buttons[i].addEventListener('click',function(){
+              if(text=='+'||text=='-'||text=='*'||text=='/'||text=='%'){
+                operand1=parseFloat(display.textContent);
+                operator=text;
+                display.innerText="";
+              }else if(text=='+/-'){
+                display.innerText*=(-1);
+              }else if(text=='AC'){
+                operand1=0;
+                operand2=null;
+                operator=null;
+                display.innerText="";
+              }else if(text=='='){
+                operand2=parseFloat(display.textContent);                
+               operand1=eval(operand1+" "+operator+" "+operand2);
+               if(operand1==Infinity||(!operand1)) operand1='Error';
+               display.innerText=operand1;
+              }else{
+                display.innerText+=text;
+              }
+            
+
+      });
+}
